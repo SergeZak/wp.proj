eadee06d138de6e7d073a583ab340e1ceaa65efb
= <=>
2016-03-22 07:54:13 +0200

    customized style for header and footer

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head <?php language_attributes()?> >
-<meta charset="<?php bloginfo('charset') ?>">
-<meta name="viewport" content="width=device-width">
-<title><?php bloginfo('name')?></title>
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php bloginfo('name')?></title>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class()?> >
+
+<div class="container">
+
+    <header class="site-header">
+        <h1><a href="<?php echo home_url() ?>"><?php bloginfo('name')?></a></h1>
+        <h5><?php bloginfo('description')?></h5>
+    </header>
