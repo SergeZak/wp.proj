819916b2018eb0fbdb8b9c4fce36918075239728
= <=>
2016-03-27 17:29:44 +0300

    added categories and author on default blog archive page

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -7,6 +7,26 @@ if(have_posts()){
         the_post(); ?>
         <article class="post">
             <h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
+
+            <p class="post-info">
+                <?php the_time('F jS, Y g:i a') ?> |
+                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" ><?php the_author() ?></a> |
+                posted in
+                <?php
+                $categories = get_the_category();
+                $separator = ", ";
+                $output = '';
+
+                if($categories){
+                    foreach ($categories as $category) {
+                        $output .=  '<a href="'. get_category_link($category->term_id) .'">'. $category->cat_name . '</a>' . $separator;
+                    }
+                    echo trim($output, ', ');
+                }
+
+                ?>
+            </p>
+
             <?php the_content() ?>
         </article>
     <?php }
