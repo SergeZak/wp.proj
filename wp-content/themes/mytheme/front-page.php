202699f2619fb2bd8ca3ad21f6ccee1b3d7cbb20
sergezak <sergezak9@gmail.com>
2016-04-11 07:47:52 +0300

    wp_query

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -19,6 +19,51 @@ get_header();
             <?php }
             ?>
 
+        <div class="home-columns">
+            <div class="one-half">
+                <?php
+                // Opinion post loop
+
+                $args = [
+                    'category_name' => 'opinion',
+                    'posts_per_page' => 2,
+                ];
+                $opinion_posts = new WP_Query($args);
+
+                if($opinion_posts->have_posts()){
+                    while ($opinion_posts->have_posts()){
+                        $opinion_posts->the_post(); ?>
+                        <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
+                        <p><?php the_excerpt() ?></p>
+                    <?php }
+                }
+                wp_reset_postdata();
+                ?>
+            </div>
+            <div class="one-half last">
+                <?php
+                // Coding post loop
+
+                $args = [
+                    'category_name' => 'coding',
+                    'posts_per_page' => 2,
+                ];
+                $coding_posts = new WP_Query($args);
+
+                if($coding_posts->have_posts()){
+                    while ($coding_posts->have_posts()){
+                        $coding_posts->the_post(); ?>
+                        <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
+                        <p><?php the_excerpt() ?></p>
+                    <?php }
+                }
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+        
+
+
         
 
 
